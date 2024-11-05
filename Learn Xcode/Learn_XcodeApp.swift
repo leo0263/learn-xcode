fb22d54066f5eb2c997711ccb0c816dccdc3b580
Leonardi <leo.0263@gmail.com>
2024-11-05 19:43:11 +0700

    Initial Commit + gitignore

---
+++ Learn Xcode/Learn_XcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_XcodeApp.swift
+//  Learn Xcode
+//
+//  Created by Leonardi on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_XcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Learn XcodeTests/Learn_XcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_XcodeTests.swift
+//  Learn XcodeTests
+//
+//  Created by Leonardi on 05/11/24.
+//
+
+import Testing
+@testable import Learn_Xcode
+
+struct Learn_XcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
