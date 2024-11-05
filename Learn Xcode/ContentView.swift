00ae9c89c8cdb6d77bb7a27fcc0061f210184858
Leonardi <leo.0263@gmail.com>
2024-11-05 21:47:10 +0700

    move github fetch/ui to its own view

--- Learn Xcode/ContentView.swift
+++ Learn Xcode/ContentView.swift
@@ -8,81 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var user: GitHubUser?
-    
     var body: some View {
-        VStack(spacing: 20) {
-            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-            } placeholder: {
-                Circle()
-                    .foregroundColor(.secondary)
-            }
-            .frame(width: 120, height: 120)
-            
-            Text(user?.login ?? "Login placeHolder")
-                .bold()
-                .font(.title3)
-            
-            Text(user?.bio ?? "Bio placeHolder")
-                .padding()
-            
-            Spacer()
-        }
-        .padding()
-        .task {
-            do {
-                user = try await getGitHubUser()
-            } catch GitHubError.invalidURL {
-                print("Invalid URL")
-            } catch GitHubError.invalidResponse {
-                print("Invalid response")
-            } catch GitHubError.invalidData {
-                print("Invalid data")
-            } catch {
-                print("Unexpected error!")
-            }
-        }
-    }
-    
-    func getGitHubUser() async throws -> GitHubUser {
-        let endpoint = "https://api.github.com/users/leo0263"
-        guard let url = URL(string: endpoint) else {
-            throw GitHubError.invalidURL
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        guard let respose = response as? HTTPURLResponse, respose.statusCode == 200 else {
-            throw GitHubError.invalidResponse
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            
-            return try decoder.decode(GitHubUser.self, from: data)
-        } catch {
-            throw GitHubError.invalidData
+        VStack {
+            GitHubView()
         }
     }
 }
 
 #Preview {
     ContentView()
-}
-
-struct GitHubUser: Codable {
-    let login: String
-    let avatarUrl: String
-    let bio: String
-}
-
-enum GitHubError: Error {
-    case invalidURL
-    case invalidResponse
-    case invalidData
 }
